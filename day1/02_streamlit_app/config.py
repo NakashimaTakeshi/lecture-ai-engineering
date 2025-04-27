332392a3448a4e552816b667b9c55bc2ab6fc914
NakashimaTakeshi <nakashima.takeshi@em.ci.ritsumei.ac.jp>
2025-04-27 16:03:35 +0000

    モデルの変更

--- day1/02_streamlit_app/config.py
+++ day1/02_streamlit_app/config.py
@@ -1,3 +1,14 @@
 # config.py
 DB_FILE = "chat_feedback.db"
-MODEL_NAME = "google/gemma-2-2b-jpn-it"
+
+# Model leaderboard
+# https://wandb.ai/llm-jp-eval/open-llm-leaderboard/reports/-Open-LLM-Leaderboard--Vmlldzo2MTE0NDA3
+# https://huggingface.co/spaces/llm-jp/open-japanese-llm-leaderboard
+# https://huggingface.co/spaces/open-llm-leaderboard/open_llm_leaderboard
+
+# MODEL_NAME = "google/gemma-2-2b-jpn-it"
+# MODEL_NAME = "google/gemma-3-27b-it"
+# →モデルが大きすぎてGPUに乗らない
+# MODEL_NAME = "google/gemma-3-1b-pt"
+# →pretrainingモデル (pt)にはトークナイザーにチャットテンプレートがない
+MODEL_NAME = "google/gemma-3-1b-it"
